bb5e0baf1435bdee30d813fe6abaa09c177bd257
emzhran <zachran750@gmail.com>
2023-04-03 10:50:08 +0700

    int main (2)

--- ActivityStruct(2)/ActivityStruct(2).cpp
+++ ActivityStruct(2)/ActivityStruct(2).cpp
@@ -14,3 +14,26 @@ struct Mahasiswa
 	AlamatDetail alamat;
 	int umur;
 };
+
+int main() {
+	Mahasiswa mhs;
+
+	cout << "Masukkan NIM :";
+	cin >> mhs.NIM;
+	cout << "Masukkan Nama :";
+	cin >> mhs.nama;
+	cout << "Alamat :";
+	cout << "\tMasukkan Desa :";
+	cin >> mhs.alamat.desa;
+	cout << "\tMasukkan Kota :";
+	cin >> mhs.alamat.kota;
+	cout << "Masukkan Umur :";
+	cin >> mhs.umur;
+
+	cout << "\n\nNIM = " << mhs.NIM;
+	cout << "\nNama = " << mhs.nama;
+	cout << "\nDesa = " << mhs.alamat.desa;
+	cout << "\nKota = " << mhs.alamat.kota;
+	cout << "\nUmur = " << mhs.umur;
+
+}
